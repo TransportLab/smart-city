ac653eccde3a25d5adb45b15ff619152aca05eb0
Benjy Marks <benjy.marks@sydney.edu.au>
2024-08-29 15:39:01 +1000

    added model aspect ratio and vertical offset to allow for on-site calibration via params file

--- sys/webhook.sh
+++ sys/webhook.sh
@@ -2,4 +2,4 @@
 
 IPADDRESS=$(curl ifconfig.me)
 
-curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "'"Smart City installation ${hostname} has turned back on with IP Address ${IPADDRESS} :tada:"'"}' https://mattermost.scigem.com/hooks/5cwtdg8cupds5n5hiehby75mwe
+curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "'"Smart City installation ${hostname} has turned back on with IP Address ${IPADDRESS} :tada:"'"}' https://mattermost.scigem.com/hooks/ubogx1hww3b1dckf4b9di6pmjr
